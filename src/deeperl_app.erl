db3704ba4ead29738da6ebb18c187556e53567a5
Alexander Wühr <l-x@mailbox.org>
2021-10-05 20:50:48 +0200

    Straighten deeperl_app

--- src/deeperl_app.erl
+++ src/deeperl_app.erl
@@ -9,19 +9,9 @@
 ]).
 
 start(_StartType, _StartArgs) ->
-    AuthKey = case application:get_env(auth_key) of
-                  {ok, X} -> X;
-                  undefined -> ""
-              end,
-
-    HttpcProfile = case application:get_env(httpc_profile) of
-                  {ok, Y} -> Y;
-                  undefined -> default
-              end,
-
     deeperl_sup:start_link(#{
-        auth_key => AuthKey,
-        httpc_profile => HttpcProfile
+        auth_key => application:get_env(deeperl, auth_key, ""),
+        httpc_profile => application:get_env(deeperl, httpc_profile, default)
     }).
 
 stop(_State) ->
